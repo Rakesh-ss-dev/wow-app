134c05d6fcb74bab9ef070b0abad6b0fffc741de
Rakesh Singh <rakesh.s@skillsigma.com>
2025-02-05 11:59:39 +0530

    removing flowbite from viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import flowbite from 'flowbite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),flowbite()],
+  plugins: [react(),tailwindcss()],
 })
