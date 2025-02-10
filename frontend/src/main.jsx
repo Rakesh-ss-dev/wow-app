d8093df8b74c7dd94b634412fa38dc0091811425
Rakesh Singh <rakesh.s@skillsigma.com>
2025-02-10 15:17:30 +0530

    changes to git

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,9 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
-
         <Route path="/" element={<Login />} />
-        
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
