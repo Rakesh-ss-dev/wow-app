64ccbfbffad5ad5ab3c93d9e694fbc347ef42694
Rakesh Singh <rakesh.s@skillsigma.com>
2025-02-10 15:24:18 +0530

    changes to elastic ip

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,8 +8,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
+
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        
       </Routes>
     </Router>
   </React.StrictMode>
