b9585ca3a22f42fba7b11a995c70b73d3ab8b6ba
Rakesh Singh <rakesh.s@skillsigma.com>
2025-02-10 14:53:49 +0530

    changes in react .env

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
+
         <Route path="/" element={<Login />} />
+        
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
