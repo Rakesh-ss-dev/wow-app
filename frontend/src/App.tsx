4b78ba6b70d145912be3e43428c1c13cfdd09593
Rakesh-ss-dev <rakesh.s@skillsigma.com>
2025-02-22 20:34:25 +0530

    made the dashboard blank page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,6 @@ import AuthLayout from "./layout/AuthLayout";
 import SignIn from "./pages/AuthPages/SignIn";
 import Blank from "./pages/Blank";
 import Calendar from "./pages/Calendar";
-import Ecommerce from "./pages/Dashboard/ECommerce";
 import FormElements from "./pages/Forms/FormElements";
 import NotFound from "./pages/OtherPage/NotFound";
 import BasicTables from "./pages/Tables/BasicTables";
@@ -17,7 +16,7 @@ import Videos from "./pages/UiElements/Videos";
 import UserProfiles from "./pages/UserProfiles";
 import RequestList from "./pages/Requests/RequestList";
 import UserDataTableRes from "./pages/UserDataTable/UserDataTableRes";
-import CreateRequest from './pages/Requests/CreateRequest';
+import CreateRequest from "./pages/Requests/CreateRequest";
 import CreateCoach from "./pages/UserDataTable/CreateCoach";
 
 export default function App() {
@@ -27,14 +26,14 @@ export default function App() {
         <Routes>
           {/* Dashboard Layout */}
           <Route element={<AppLayout />}>
-            <Route index path="/Dashboard" element={<Ecommerce />} />
+            <Route index path="/Dashboard" element={<Blank />} />
             {/* Others Page */}
             <Route path="/profile" element={<UserProfiles />} />
             <Route path="/calendar" element={<Calendar />} />
             <Route path="/blank" element={<Blank />} />
-            <Route path="/requests" element={<RequestList/>}/>
-            <Route path="/create-request" element={<CreateRequest/>}/>
-            
+            <Route path="/requests" element={<RequestList />} />
+            <Route path="/create-request" element={<CreateRequest />} />
+
             {/* Forms */}
             <Route path="/form-elements" element={<FormElements />} />
 
@@ -42,8 +41,6 @@ export default function App() {
             <Route path="/basic-tables" element={<BasicTables />} />
             <Route path="/coaches" element={<UserDataTableRes />} />
             <Route path="/create-coach" element={<CreateCoach />} />
-
-            
 
             {/* Ui Elements */}
             <Route path="/alerts" element={<Alerts />} />
