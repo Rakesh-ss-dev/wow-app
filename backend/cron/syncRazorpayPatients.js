3efc11331f644c5ec178f188ce19c4b96f5ef9bb
rakesh <rakesh.s@skillsigma.com>
2025-06-09 11:23:22 +0530

    added paid in cronjobs

--- backend/cron/syncRazorpayPatients.js
+++ backend/cron/syncRazorpayPatients.js
@@ -31,6 +31,7 @@ async function syncRazorpayToPatients() {
     const patients = await Patients.find({
         $or: [
           { status: "created" },
+          { status: "paid" },
           { status: { $exists: false } },
           { status: "" },
           { status: null },
