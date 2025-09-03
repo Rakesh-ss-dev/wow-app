37a3f285c8ddc565a358d0282bd6e351315c78ff
rakesh <rakesh.s@skillsigma.com>
2025-09-03 17:34:59 +0530

    changes to erroe

--- backend/routes/client.js
+++ backend/routes/client.js
@@ -31,7 +31,7 @@ router.post("/register", async (req, res) => {
     await patient.save();
     res.status(201).json({ message: "User registered successfully." });
   } catch (error) {
-    res.status(500).json({ error: "Server error" });
+    res.status(500).json({ error: error });
   }
 });
 
