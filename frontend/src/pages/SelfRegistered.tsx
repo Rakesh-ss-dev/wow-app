4e21171ae16c203b185d05eb323c6ebbac02d5fe
rakesh <rakesh.s@skillsigma.com>
2025-09-16 17:48:59 +0530

    search in self registerd

--- frontend/src/pages/SelfRegistered.tsx
+++ frontend/src/pages/SelfRegistered.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import axiosInstance from "../api/axios";
 import SelfRegisteredTable from "../components/datatables/SelfRegisteredTable";
+import { filterRequests } from "../utils/search";
+import Input from "../components/form/input/InputField";
 
 type Request = {
     _id: string;
@@ -14,7 +16,8 @@ type Request = {
 const SelfRegistered = () => {
     const [requests, setRequests] = useState<Request[]>([]);
     const [loading, setLoading] = useState(true);
-
+    const [filteredRequests, setFilteredRequests] = useState<any[]>([]);
+    const [searchTerm, setSearchTerm] = useState("");
     const fetchRequests = async () => {
         try {
             const response = await axiosInstance.get("/payment/getSelfRegistered");
@@ -29,13 +32,19 @@ const SelfRegistered = () => {
     useEffect(() => {
         fetchRequests();
     }, []); // fetch once on mount
-
+    useEffect(() => {
+        setFilteredRequests(filterRequests(requests, searchTerm, ["name", "phone"]));
+    }, [searchTerm, requests]);
     return (
         <div className="p-6">
             {loading ? (
                 <p>Loading...</p>
             ) : (
-                <SelfRegisteredTable data={requests} />
+                <div className="w-full mb-4">
+                    <Input className="w-full" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search User name or Mobile Number" />
+                    <SelfRegisteredTable data={filteredRequests} />
+                </div>
+
             )}
         </div>
     );
