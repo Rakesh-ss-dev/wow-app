aad14b129e392b334b89fe83678277753eb7a53e
rakesh <rakesh.s@skillsigma.com>
2025-12-05 12:49:42 +0530

    save all

--- frontend/src/pages/Requests/CoachClients.tsx
+++ frontend/src/pages/Requests/CoachClients.tsx
@@ -57,13 +57,13 @@ const CoachClients = () => {
                     </div>
 
                     {loading ? (
-                        <p className="text-gray-600">Loading requests...</p>
+                        <p className="text-gray-600">Loading Clients...</p>
                     ) : error ? (
                         <p className="text-red-500">{error}</p>
                     ) : requests.length > 0 ? (
                         <RequestDataTable data={filteredRequests} />
                     ) : (
-                        <p className="text-gray-600">No requests available</p>
+                        <p className="text-gray-600">No clients available</p>
                     )}
                 </div>
             </ComponentCard>
